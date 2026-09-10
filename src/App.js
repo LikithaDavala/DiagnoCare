d3303c314fd20105b2baab6e86b81de2e1a79514
LikithaDavala <likithadavala59@gmail.com>
2026-09-10 17:14:12 +0530

    App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import RegistrationForm from "./Pages/RegistrationForm/RegistrationForm";
+import RegistrationForm from "./Pages/RegistrationForm/registrationForm";
+import LoginPage from "./Pages/LoginPage/loginPage";
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<RegistrationForm />} />
+        <Route path="/login" element={<LoginPage />} /> 
       </Routes>
     </BrowserRouter>
   );
